4bcd0e9e40fd5d980448e4a3988aaa0e7da087fa
muhammadlovlu <chyshahab@gmail.com>
2021-02-08 13:09:29 +0600

    updated

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,21 @@
+const first = 2;
+const second = 2;
+
+if (first == second){
+    console.log(" first-second variable 'double equal' condition is true");
+}
+else {
+    console.log("first-second variable 'double equal' condition is false");
+}
+
+///////
+const firstExample = 14;
+const secondExample = "14";
+if (firstExample === secondExample){
+    console.log("firstExample-secondExample variable condition is true");
+}
+else {
+    console.log("firstExample-secondExample variable condition is false")
+}
+
+
