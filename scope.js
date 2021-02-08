9536068651d2f9b0fc5ed4508c41fbacffd6380c
muhammadlovlu <chyshahab@gmail.com>
2021-02-09 01:09:34 +0600

    updated

---
+++ scope.js
@@ -0,0 +1,22 @@
+
+function sum(first, second) {
+    var number = 4;
+    const total = first + second;
+    //console.log(number);
+    return total;
+    
+}
+
+const output = sum(4, 4);
+console.log(output);
+
+// console.log(number);      // output will be error
+
+
+// console.log(number);
+// let number = 8;          // output will be error
+
+
+
+// var number = 12; 
+// console.log(number);  // output 12
