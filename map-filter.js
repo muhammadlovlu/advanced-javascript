4d6bc962d454cdbe0c9e7059bbb10f68653973e0
muhammadlovlu <chyshahab@gmail.com>
2021-02-08 13:46:44 +0600

    updated

---
+++ map-filter.js
@@ -0,0 +1,19 @@
+const numbers = [45,50,55,60,65,70, 75, 80];
+const output = [];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);                              
+}
+console.log(output);
+ 
+
+////////////////////
+const numbersTwo = [2,3,4,5,6,7,8,9];
+const outputTwo = [];
+for (let i = 0; i < numbersTwo.length; i++) {
+    const element = numbersTwo[i];
+    const resultTwo = element * element;
+    outputTwo.push(resultTwo)
+}
+console.log(outputTwo);
