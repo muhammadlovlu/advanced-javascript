1ce4f6605e516a1c446b4c4446bea98e77123b52
muhammadlovlu <chyshahab@gmail.com>
2021-02-08 12:08:10 +0600

    updated

---
+++ true-false.js
@@ -0,0 +1,99 @@
+// If any digit from (1-9)
+const age = 4;
+if (age){
+    console.log('Condition is true');
+}
+else {
+    console.log('Condition is false');
+}
+
+
+//  if the value is zero 
+const zeroDigit = 0;
+if (zeroDigit){
+    console.log("conditon Zero is True");
+}
+else {
+    console.log('Condition zero is False');
+}
+
+
+// if empty string
+const emptyString = "";
+if (emptyString){
+    console.log('emptystring is true');
+}
+else{
+    console.log('emptystring is false');
+}
+
+
+// emptystring but space inside  will be true
+const space = " ";
+if(space){
+    console.log('space inside emptystring is true');
+}
+else {
+    console.log('space inside emptystring is false');
+}
+
+
+//////////////////////
+const personName = "Robart";
+if (personName.length>5){
+    console.log('PersonName variable is true');
+}
+else {
+    console.log('personName variable is false');
+}
+
+
+///////////////
+const secondPerson = "David Arnold";
+if(secondPerson.length>11){
+    console.log('secondPerson condition is true');
+}
+else {
+    console.log('secondPerson condition is false');
+}
+
+
+//////////////////////
+const thirdPerson = "Nick Harrison Thomas";
+if(thirdPerson.length>19){
+    console.log('thirdPerson variable condition is true');
+}
+else{
+    console.log('thirdPerson variable condition is false');
+}
+
+
+////////////////////////
+const numberCheck = 0;
+if(numberCheck || numberCheck ==0 ){
+    console.log('numberCheck variable condition is true');
+}
+else {
+    console.log('numberCheck variable is false');
+}
+
+
+///////////////////////
+const numbercheck = null ;
+if (numbercheck || numbercheck == null) {
+    console.log('numbercheck condition is true');
+}
+else {
+    console.log('numbercheck condition is false');
+}
+
+
+//////////////////
+const emptyArray = [];
+if (emptyArray){
+    console.log('emptyArray variable condition is true ')
+
+} 
+else {
+    console.log('emptyArray Variable condition is false');
+}
