a343354a953914a1246ee2cea79122e5d89961ec
muhammadlovlu <chyshahab@gmail.com>
2021-02-15 20:48:11 +0600

    updated

--- callApply.js
+++ callApply.js
@@ -4,6 +4,29 @@ const person = {                       //an object
     salary: 15000,
     fullName: function () {
         console.log(this.firstName, this.lastName)
-    }
+    },
+    chargeBill : function (amount){
+        this.salary = this.salary - amount;
+        return this.salary;
+    },
+    addAmount : function (addMoney){
+        this.salary = this.salary + addMoney;
+        return this.salary;
+    },
+    
 }
-console.log(person.firstName, person.lastName)  // access object's property
+////////////////////////////////////////////////////////
+person.chargeBill(50);          // withdraw 50 money
+person.chargeBill(4950);        // withdraw 4950 money
+console.log(person.salary);
+
+
+///////////////////////////////////////////////////////
+person.addAmount(2000);     // deposit 2000 
+console.log(person.salary); // current balance
+person.addAmount(8000);     // deposit 8000 
+console.log(person.salary); // current balance
+
+
+//////////////////////////////////////////////////////
+
