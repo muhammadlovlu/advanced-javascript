2a73f2b06c0dd50f8bbff934bd5d2a798681ba0b
muhammadlovlu <chyshahab@gmail.com>
2021-02-15 13:16:34 +0600

    updated

--- callApply.js
+++ callApply.js
@@ -1,10 +1,9 @@
 const person = {                       //an object
-    firstName : 'Rick',
-    lastName : 'Dan',
+    firstName: 'Rick',
+    lastName: 'Dan',
     salary: 15000,
-    fullName : function(){
-        console.log(this.firstName,this.lastName)
+    fullName: function () {
+        console.log(this.firstName, this.lastName)
     }
 }
-
 console.log(person.firstName, person.lastName)  // access object's property
