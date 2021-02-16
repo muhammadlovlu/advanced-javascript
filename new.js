ca4663a92c6c59fbd2f5bb0335a2fda81d3778f3
muhammadlovlu <chyshahab@gmail.com>
2021-02-16 17:44:54 +0600

    updated

---
+++ new.js
@@ -0,0 +1,46 @@
+class Person {
+    constructor (firstName, lastName, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+
+
+// first person = create an array with class 
+const firstPerson = new Person ("Arselian", "Dav", 10000);
+console.log("Object created First", firstPerson);
+
+
+// second person  = create an array with class
+const secondPerson = new Person ("Richard", "Arnold", 15000);
+console.log("Object created Second",secondPerson);
+
+
+//////////////////////////////////////////////////////////////
+
+
+// Creating neighbour details with class 
+
+class Details {
+    constructor (Firstname, Lastname, Wifename,Child,Salary, House,Car, DailyCost,Savings){
+        this.firstName = Firstname ;
+        this.lastName = Lastname;
+        this.wifeName = Wifename ;
+        this.children = Child;
+        this.salary = Salary ;
+        this.house  = House;
+        this.car = Car;
+        this.dailyExpencess = DailyCost;
+        this.savings = Savings;
+    }
+}
+
+const neighbourOne = new Details ("Nick", "Harrison", "Dalia", 4, 20000, "Yes", 2, 2000, 5000);
+console.log("neighbourOne",neighbourOne);
+
+const neighbourTwo = new Details ("Daniel", "Vron", "Samia", 2, 25000, "Yes", 5, 1000,3000);
+console.log("NeighbourTwo",neighbourTwo);
+
+const neighbourThree = new Details ("Hammad", "Clark", "Nisa" ,5, 30000, "Yes", 1, 1200, 7000);
+console.log("NeighbourThree",neighbourThree)
--- window.js
+++ window.js
@@ -1,14 +1,16 @@
 var name = "Dan";
 
-function sum(num1, num2) {
-    const result = num1 + num2 ;
+function add(num1, num2) {
+    var result = num1 + num2 ;
     return result;
     
-    function sum2(num1, num2){
+    function add2(num1, num2){
         const resutl2 = num1 - num2;
         return resutl2;
     }
 }
 
-console.log(sum(10,20));
-console.log(sum2(50,20))
+
+add(12,15);
+console.log(add(10,20));
+console.log(add2(50,20))
