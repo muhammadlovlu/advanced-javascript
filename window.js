61a7305d2a4288433b9fc41cf5732da25293673e
muhammadlovlu <chyshahab@gmail.com>
2021-02-16 16:26:15 +0600

    updated

---
+++ window.js
@@ -0,0 +1,14 @@
+var name = "Dan";
+
+function sum(num1, num2) {
+    const result = num1 + num2 ;
+    return result;
+    
+    function sum2(num1, num2){
+        const resutl2 = num1 - num2;
+        return resutl2;
+    }
+}
+
+console.log(sum(10,20));
+console.log(sum2(50,20))
