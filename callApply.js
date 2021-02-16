785192d7e60887796ecd4d127ee04cee66c45871
muhammadlovlu <chyshahab@gmail.com>
2021-02-16 15:27:08 +0600

    updated

--- callApply.js
+++ callApply.js
@@ -17,13 +17,13 @@ const person = {                       //an object
 }
 person.chargeBill(50);          // withdraw 50 money
 person.chargeBill(4950);        // withdraw 4950 money
-console.log(person.salary);
+console.log("Person's withdraw amount :",person.salary);
 
 person.addAmount(2000);     // deposit 2000 
-console.log(person.salary); // current balance
+console.log("Person's Deposited :",person.salary); // current balance
 
 person.addAmount(8000);     // deposit 8000 
-console.log(person.salary); // current balance
+console.log("Person's Deposit :",person.salary); // current balance
 
 
 
@@ -36,9 +36,9 @@ const secondPerson = {
     salary: 25000
 }
 const secondChargeBill = person.chargeBill.bind(secondPerson);
-console.log(secondChargeBill(1000));
+console.log("Second Person withdraw Amount :", secondChargeBill(1000));  // withdraw amount 1000
 const secondAddMoney = person.addAmount.bind(secondPerson);
-console.log(secondAddMoney(6000));
+console.log("Second Person Deposit Amount :", secondAddMoney(6000));   // deposit amount 6000
 
 
 // thirdPerson example for bind method
@@ -49,9 +49,9 @@ const thirdPerson = {
     salary: 40000
 }
 const thirdChargeBill = person.chargeBill.bind(thirdPerson);
-console.log(thirdChargeBill(20000));
+console.log("Third Person withdraw Amount :", thirdChargeBill(20000));  // withdraw amount 20000
 const thirdAddMoney = person.addAmount.bind(thirdPerson);
-console.log(thirdAddMoney(30000));
+console.log("Third Person deposit Amount :", thirdAddMoney(30000));     //deposit amount 30000
 
 
 // forthPerson example for bind method
@@ -62,6 +62,20 @@ const forthPerson = {
     salary: 70000
 }
 const forthChargeBill = person.chargeBill.bind(forthPerson);
-console.log(forthChargeBill(5000));
-const forthAddMoney = person.addAmount.bind(forthPerson);
-console.log(forthAddMoney(15000));
+console.log("Forth Person withdraw Amount :",forthChargeBill(5000));
+const forthAddMoney = person.addAmount.bind(forthPerson);               // withdraw amount 5000
+console.log("Forth Person deposit Amount :",forthAddMoney(15000));    // deposit amount 15000
+
+
+// fifthPerson example for call method
+const fifthPerson = {
+    firstName: 'Sam',
+    lastName: 'Daniel',
+    favFood: 'pizza',
+    salary: 40000
+}
+
+person.chargeBill.call(fifthPerson, 2000);
+console.log("Fifth Person's charges :",fifthPerson.salary);
+person.addAmount.call(fifthPerson,12000);
+console.log("Fifth Person's Deposit :",fifthPerson.salary);
